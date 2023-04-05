f86f297b47c1c39439b78d8f890c35f9b249a936
Sivapurni Ramendran <sivapurni@gmail.com>
2023-04-05 12:29:20 +0800

    Comment

--- app/src/main/java/com/sivapurni/myhealthapp/BloodPressureResult.java
+++ app/src/main/java/com/sivapurni/myhealthapp/BloodPressureResult.java
@@ -14,26 +14,26 @@ public class BloodPressureResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure_result);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Blood Pressure");
-        actionBar.setSubtitle("Dashboard");
-        back=findViewById(R.id.btnBack);
-        disp=findViewById(R.id.tvDisp);
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setTitle("Blood Pressure");
+        //actionBar.setSubtitle("Dashboard");
+        //back=findViewById(R.id.btnBack);
+        //disp=findViewById(R.id.tvDisp);
         // getting the bundle from the intent
-        Bundle bundle = getIntent().getExtras();
+        //Bundle bundle = getIntent().getExtras();
         // setting the text in the textview
-        weight=bundle.getDouble("berat");
-        height=bundle.getDouble("tinggi");
-        bmr=bundle.getDouble("bmr");
-        umur=bundle.getInt("umur");
-        disp.setText("Weight: "+weight+"kg\nHeight: "+height+"cm\nAge: "+umur+"yrs\nBMR: "+bmr+" calories/day");
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), BMRActivity.class);
-                startActivity(intent);
-            }
-        });
+       // weight=bundle.getDouble("berat");
+        //height=bundle.getDouble("tinggi");
+        //bmr=bundle.getDouble("bmr");
+        //umur=bundle.getInt("umur");
+        //disp.setText("Weight: "+weight+"kg\nHeight: "+height+"cm\nAge: "+umur+"yrs\nBMR: "+bmr+" calories/day");
+        //back.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View view) {
+             //   Intent intent=new Intent(getApplicationContext(), BMRActivity.class);
+             //   startActivity(intent);
+           // }
+      //  });
 
     }
 }
